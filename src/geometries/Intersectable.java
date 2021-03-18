e68180392f4c9906fa6e7f05f91b886f897ff716
אביחי סוריה <soria@g.jct.ac.il>
2021-03-18 21:38:24 +0200

    3.1

--- src/geometries/Intersectable.java
+++ src/geometries/Intersectable.java
@@ -5,6 +5,14 @@ import primitives.Ray;
 
 import java.util.List;
 
+/**
+ * This interface includes one method that finds the intersections between the current object a ray
+ */
 public interface Intersectable {
+    /**
+     * This method recives a ray and returns a list of all the interction points. In case there are none, null will be returned
+     * @param ray The ray which we find the intersections to the object
+     * @return A list of the intersection points in form of Point3D. In case there are no intersections, null will be returned
+     */
     List<Point3D> findIntersections(Ray ray);
 }

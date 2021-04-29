2b32681bbaed95cf6dd76ee249fe41f629349f59
אביחי סוריה <soria@g.jct.ac.il>
2021-04-29 17:01:25 +0300

    5.3 some more documentations

--- src/renderer/Render.java
+++ src/renderer/Render.java
@@ -6,14 +6,24 @@ import scene.*;
 
 import java.util.MissingResourceException;
 
+/***
+ * This class includes the fields Scene, Camera, RayTracerBase and ImageWriter.
+ * It contains setters and methods for rendering.
+ */
 public class Render {
 
+    //Note
     private Scene scene = null;
     private Camera camera = null;
     private RayTracerBase rayTracer = null;
     private ImageWriter imageWriter = null;
 
 
+    /***
+     * This method get all the rays from the camera to each pixel, for each ray receives a color from the RayTracerBasic
+     * then draws the checked pixel with the color we received.
+     * @throws MissingResourceException in case the any of the fields is null.
+     */
     public void renderImage(){
 
         if(scene == null || camera == null|| rayTracer== null|| imageWriter== null)
@@ -22,6 +32,12 @@ public class Render {
 
     }
 
+    /***
+     * This method receives an interval of distance and a color to draw lines on the image, mostly used to testing purposes.
+     * @param interval The amount of pixels between each line in the grid.
+     * @param color The color of the grid's line.
+     * @throws MissingResourceException in case the imageWriter is null.
+     */
     public void printGrid(int interval, Color color){
         if(imageWriter == null)
             throw new MissingResourceException("A","B","C");
@@ -43,6 +59,10 @@ public class Render {
 
     }
 
+    /***
+     * This method is being used to call the writeToImage method of the field imageWriter.
+     * @throws MissingResourceException in case the imageWriter is null.
+     */
     public void writeToImage(){
         if(imageWriter ==null)
             throw new MissingResourceException("A","B","C");
@@ -50,22 +70,37 @@ public class Render {
         imageWriter.writeToImage();
     }
 
-
+    /***
+     * Setter for the Scene field of this Render.
+     * @return This Render.
+     */
     public Render setScene(Scene scene) {
         this.scene = scene;
         return this;
     }
 
+    /***
+     * Setter for the Camera field of this Render.
+     * @return This Render.
+     */
     public Render setCamera(Camera camera) {
         this.camera = camera;
         return this;
     }
 
+    /***
+     * Setter for the RayTracer field of this Render.
+     * @return This Render.
+     */
     public Render setRayTracer(RayTracerBase rayTracer) {
         this.rayTracer = rayTracer;
         return this;
     }
 
+    /***
+     * Setter for the ImageWriter field of this Render.
+     * @return This Render.
+     */
     public Render setImageWriter(ImageWriter imageWriter) {
         this.imageWriter = imageWriter;
         return this;

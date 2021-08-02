d9e7e7f1918b6552caf1de8aa89beeaea701b2f5
אבישי אליחי <avishayelihay@gmail.com>
2021-08-02 20:59:01 +0300

    optimal intersection by Triangle with no memory addition!!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -19,7 +19,7 @@ public final class Main {
 
 
     public static double V = 0.6;
-    public static Point3D p = new Point3D(0.46, -0.5, 0.9);
+    public static Point3D p = new Point3D(0.54, -0.5, 1.1);
 
     /**
      * Main program to tests initial functionality of the 1st stage
@@ -32,11 +32,12 @@ public final class Main {
         Point3D p2 = new Point3D(0, 0, 2);
         Point3D p3 = new Point3D(1, -1, 0);
         Triangle triangle = new Triangle(p1, p2, p3);
-        Vector temp12 = p1.subtract(p2);
-        Vector temp23 = p2.subtract(p3);
-        Vector temp31 = p3.subtract(p1);
-        Vector temp = p.subtract(p1);
-        out.println(0.46+1.8*-0.5+0.9*-0.4);
+        Vector temp1 = p1.subtract(p);
+        Vector temp2 = p2.subtract(p);
+        Vector temp3 = p3.subtract(p);
+        out.println(temp1.crossProductValue(temp2));
+        out.println(temp2.crossProductValue(temp3));
+        out.println(temp3.crossProductValue(temp1));
 
     }
 
--- src/geometries/Triangle.java
+++ src/geometries/Triangle.java
@@ -20,9 +20,6 @@ public class Triangle extends Polygon {
      */
     public Triangle(Point3D point1, Point3D point2, Point3D point3) {
         super(point1, point2, point3);
-        checkLines.add(point3.subtract(point1));
-        checkLines.add(point1.subtract(point2));
-        checkLines.add(point2.subtract(point3));
     }
 
     /***
@@ -46,49 +43,8 @@ public class Triangle extends Polygon {
     }
 
     /**
-     * DAN THINKING:
-     * This method receives a ray and his max distance and returns a list of all the intersections points in objects of GeoPoint.
-     * In case there are none or pass the max distance, null will be returned.
-     *
-     * @param ray         The ray which we find the intersections to the object.
-     * @param maxDistance the maximum distance for the ray to go
-     * @return A list of the intersection points in form of GeoPoint. In case there are no intersections, null will be returned.
-     */
-  /*  @Override
-    public List<GeoPoint> findGeoIntersectionsParticular(Ray ray, double maxDistance) {
-
-        List<GeoPoint> planeIntersections = plane.findGeoIntersectionsParticular(ray, maxDistance);
-        //because we care about the distance in the plane we
-        //don't need to care about it here
-
-        if (planeIntersections == null)
-            return null;
-
-        try {
-            GeoPoint p = planeIntersections.get(0);
-
-            Vector v1 = vertices.get(0).subtract(p.point);
-            Vector v2 = vertices.get(1).subtract(p.point);
-            Vector v3 = vertices.get(2).subtract(p.point);
-
-            Vector v1t = v1.crossProduct(v2);
-            Vector v2t = v2.crossProduct(v3);
-            if (v1t.dotProduct(v2t) < 0)
-                return null;
-            Vector v3t = v3.crossProduct(v1);
-            if (v1t.dotProduct(v3t) < 0)
-                return null;
-
-            p.geometry = this;
-            return planeIntersections;
-
-        } catch (IllegalArgumentException ignore) {
-            return null;
-        }
-    }*/
-    /**
      * MY THINKING:
-     *
+     * <p>
      * This method receives a ray and his max distance and returns a list of all the intersections points in objects of GeoPoint.
      * In case there are none or pass the max distance, null will be returned.
      *
@@ -107,10 +63,13 @@ public class Triangle extends Polygon {
             return null;
         GeoPoint geo = planeIntersections.get(0);
         try {
-            double sign = geo.point.subtract(vertices.get(1)).crossProductValue(checkLines.get(1));
-            if (Util.alignZero(sign * geo.point.subtract(vertices.get(0)).crossProductValue(checkLines.get(0))) <= 0)
+            Vector vec0 = vertices.get(0).subtract(geo.point);
+            Vector vec1 = vertices.get(1).subtract(geo.point);
+            Vector vec2 = vertices.get(2).subtract(geo.point);
+            double midValue = vec1.crossProductValue(vec2);
+            if (Util.alignZero(vec0.crossProductValue(vec1) * midValue) <= 0)
                 return null;
-            if (Util.alignZero(sign * geo.point.subtract(vertices.get(2)).crossProductValue(checkLines.get(2))) <= 0)
+            if (Util.alignZero(midValue * vec2.crossProductValue(vec0)) <= 0)
                 return null;
             geo.geometry = this;
             return planeIntersections;
